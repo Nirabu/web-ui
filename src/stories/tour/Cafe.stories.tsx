6cfa1bda69a99f76918387344709597f412b9a8b
Daniel <danielsby@outlook.com>
2019-09-09 00:23:28 +0200

    Removed a few bugs.

--- src/stories/tour/Cafe.stories.tsx
+++ src/stories/tour/Cafe.stories.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import { storiesOf } from "@storybook/react";
 import { Cafe } from "../../containers/tour/cafe/Cafe";
+import {Buttons} from "../../presentations/components/multi/Buttons";
+
 
 storiesOf('Pages/Sections/Cafe', module)
-    .add(
-        'Cafe',
-        () => (
-            <Cafe />
-        )
-    );
+    .add('Ok button', () => <Buttons text={'Ok'}/>);
