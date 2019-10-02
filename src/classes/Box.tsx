a3e8364473feaff4cd88412a8ab33462e92c1664
Daniel <danielsby@outlook.com>
2019-10-02 15:48:25 +0200

    Temporary removed a few sections. Cafe, news and shopping are getting prioritized. Fixed a little bit CSS.

---
+++ src/classes/Box.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Box extends React.Component {
+    render() {
+        return (
+            <>
+
+
+            </>
+        )
+    }
+}
+
+export default Box;
---
+++ src/classes/SectionBox.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Box from "./Box";
+
+class SectionBox extends React.Component<Box> {
+    render() {
+        return (
+            <>
+
+
+            </>
+        )
+    }
+}
