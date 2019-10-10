a6efedaed70b303900e8b9ac959e89db3e527ae1
Daniel <danielsby@outlook.com>
2019-10-10 23:21:22 +0200

    Cleaned-up, on-going to improve redux and home functionality

--- src/redux/actions/sections/homeActions.tsx
+++ src/redux/actions/sections/homeActions.tsx
@@ -1,29 +1,24 @@
 import * as React from 'react';
-import {ADD_COFFEE, ADD_CAKE, DELETE_COFFEE, DELETE_CAKE} from "../constants/constants";
 
 export const addCoffee = (coffee: string) => {
     return {
-        type: ADD_COFFEE,
         coffee
     }
 };
 
 export const addCake = (cake: string) => {
     return {
-        type: ADD_CAKE,
         cake
     }
 };
 
 export const deleteCoffee = (coffee: string) => {
     return {
-        type: DELETE_COFFEE
     }
 };
 
 export const deleteCake = (cake: string) => {
     return {
-        type: DELETE_CAKE
     }
 };
 
