5cc783a1dfec3d634403bd792cbc2b234edfbbd2
Daniel <danielsby@outlook.com>
2019-11-10 21:00:56 +0100

    Made the css work again.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,5 @@
 import * as React from "react";
-/* Redux. */
-/* Layouts. */
-import DefaultFooter from "./../layouts/default/DefaultFooter";
-/* Containers. */
-import HomeCafeContainer from "../containers/home/HomeCafeContainer";
-import HomeNewsContainer from "../containers/home/HomeNewsContainer";
-import HomeShoppingContainer from "../containers/home/HomeShoppingContainer";
-import {NavAccount, NavTour} from "../components/Navigations";
-import {SearchBox} from "../components/Search";
-import {TitleMedium} from "../components/Titles";
-import {mainChapter9} from "../functions/ecmascript/chapter9";
-import {Temperature} from "../components/Weather";
+
 import Introduction from "../layouts/Introduction";
 import Boxes from "../layouts/Boxes";
 import Ending from "../layouts/Ending";
@@ -22,15 +11,15 @@ interface AppProps {
 const Home: React.FC<AppProps> = (props) => {
     return (
         <div className="home">
-            <header className="introduction">
+            <header className="home__introduction">
                 <Introduction />
             </header>
 
-            <main className="boxes">
+            <main className="home__boxes">
                 <Boxes/>
             </main>
 
-            <footer className="ending">
+            <footer className="home__ending">
                 <Ending />
             </footer>
         </div>
@@ -38,4 +27,3 @@ const Home: React.FC<AppProps> = (props) => {
 };
 
 export default Home;
-
